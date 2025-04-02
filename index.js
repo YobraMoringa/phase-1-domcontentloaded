696e958f31cf156577fe79bb7baebd033874ab03
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-04-02 15:54:42 +0300

    Added DOMContentLoaded event listener

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    const paragraph = document.querySelector("#text");
+    paragraph.textContent = "This is really cool!";
+})
+
+console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
